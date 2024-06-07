9543041f563ec5049c5b2af86ae9cc6e97445067
Shahriar Alom Fardin <mdfardin496@gmail.com>
2024-06-07 23:03:26 +0600

    background color change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navber from "./Navber";
 const App = () => {
   return (
     <>
-      <div className="h-screen bg-blue-200 dark:bg-black/50">
+      <div className="h-screen bg-green-200 dark:bg-black/50">
         <Navber />
       </div>
     </>
